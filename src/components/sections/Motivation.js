ff857d6e5726f1cca82063a8f0cf1da9abbcdff8
Olaoye Abayomi Oluwatoyin <mindprepopps@gmail.com>
2023-05-23 01:45:45 +0100

    Style Motivation section

--- src/components/sections/Motivation.js
+++ src/components/sections/Motivation.js
@@ -3,10 +3,10 @@ import Smart from '../sub/smart';
 import './css/sections.css';
 
 const Motivation = () => (
-  <section className="motivation div w--100">
+  <section className="motivation div w--100 flex column">
     <div className="sub--hero--div ml--120">
-      <h2 className="green-title-text max--text-1 border-image temp--font">What Motivates Us</h2>
-      <p className="hero--text w-6 sub-text green">
+      <h2 className="green-title-text main--title max--text-1 border-image temp--font">What Motivates Us</h2>
+      <p className="hero--text w-6 sub-text one green">
         With a mission to provide sustainable and nutritious food options to consumers,
         El-kanis and Partners has established itself as a pioneer in the agricultural
         sector by utilizing
@@ -16,14 +16,14 @@ const Motivation = () => (
     </div>
     <div className="backgd" />
     <Smart />
-    <article className="climate-art ml--120">
-      <h2 className="green-title-text temp--font">Climate Smart Technology</h2>
-      <p className="hero--text w-6 sub-text deep--green">
+    <article className="climate-art flex column ml--120">
+      <h2 className="climate--title green-title-text temp--font">Climate Smart Technology</h2>
+      <p className="sub-text green" style={{ width: '85%' }}>
         Through the use of remote sensing, satellite imagery and improved seeds, the company
-        is able to monitorand manage 64,000 hectares of rice fields in real-time, optimizing
+        is able to monitor and manage 64,000 hectares of rice fields in real-time, optimizing
         resource utilization and minimizing environmental impact.
       </p>
-      <p className="hero--text w-6 sub-text deep--green">
+      <p className="sub-text deep--green" style={{ width: '85%', marginTop: '1rem' }}>
         This approach not only ensures the production of healthy and safe rice, but also helps
         to conserve precious natural resources, making it a win-win for both consumers and
         the environment.
