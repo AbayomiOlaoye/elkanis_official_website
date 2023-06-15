27810754b5f42f046d544d939f179f94a4f31a8a
Olaoye Abayomi Oluwatoyin <mindprepopps@gmail.com>
2023-06-15 10:52:18 +0100

    Complete Hero page

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -8,17 +8,17 @@ import drone from '../../assets/generic/drones.svg';
 
 const Hero = () => (
   <div className="hero video--container d-flex column relative">
-    <video autoPlay muted loop className="media">
+    <video autoPlay muted loop className="media none--mobile">
       <source src={Video} type="video/mp4" />
     </video>
     <div className="video--overlay d-flex column a-i-c j-c-c">
-      <h1 className="hero--title temp--font">
+      <h1 className="hero--title text--left temp--font">
         Revolutionizing
         <span className="hero--bold"> food production </span>
         with technology
       </h1>
       <img src={drone} alt="Flying Drone" className="droner" />
-      <p className="hero--text white">
+      <p className="hero--text text--left white">
         El-kanis and Partners is a leading food production company that is changing the game in
         the agriculture sector in Nigeria by leveraging cutting-edge technology to produce
         high-quality rice and livestock supplements
