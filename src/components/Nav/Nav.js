47a51c5a2df717352f94651dd141d78a4af948fd
Olaoye Abayomi Oluwatoyin <mindprepopps@gmail.com>
2023-06-19 12:38:33 +0100

    Fix navigation bug

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,6 +1,6 @@
 import { AiFillCloseCircle } from 'react-icons/ai';
 import React, { useState, useEffect } from 'react';
-// import { CSSTransition } from 'react-transition-group';
+import { CSSTransition } from 'react-transition-group';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { MdOutlineMenu } from 'react-icons/md';
@@ -9,7 +9,6 @@ import { Link, Outlet } from 'react-router-dom';
 import Logo from '../../assets/logos/main_logo.svg';
 import './css/nav.css';
 import '../../index.css';
-import { SideNav } from './SideNav';
 
 const Nav = () => {
   useEffect(() => {
@@ -20,18 +19,18 @@ const Nav = () => {
   }, []);
 
   const [toggle, setToggle] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
+  // const [isMobile, setIsMobile] = useState(false);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     setIsMobile(window.innerWidth < 768);
+  //   };
 
-    handleResize();
+  //   handleResize();
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+  //   window.addEventListener('resize', handleResize);
+  //   return () => window.removeEventListener('resize', handleResize);
+  // }, []);
 
   const handleMenuOpen = () => {
     setToggle(!toggle);
@@ -45,55 +44,53 @@ const Nav = () => {
     <>
       <header className="header--container flex">
         <Link to="/" className="logo--link flex block">
-          <img src={Logo} className="logo max--100" data-aos="fade-down" alt="el-kanis & partners's logo" />
+          <img src={Logo} className="logo max--100" data-aos="fade-down" alt="elkanis & partners's logo" />
         </Link>
         {
             toggle
               ? <AiFillCloseCircle className="exit hamburger hide--desk" onClick={handleMenuClose} />
               : <MdOutlineMenu className="menu hamburger hide--desk" onClick={handleMenuOpen} />
           }
-        {/* <CSSTransition
+        <CSSTransition
           in={toggle}
           timeout={300}
           classNames="slide"
-          unmountOnExit
-        > */}
-        <nav className={`nav--container ${toggle ? '' : 'hide--mob'}`}>
-          <ul className="nav--list un-list flex" data-aos="fade-left">
-            <li className="nav--item--container list">
-              <Link to="/" className="nav--item" data-aos="fade-up" onClick={handleMenuClose}>Home</Link>
-            </li>
-            <li className="nav--item--container list">
-              <Link to="/about-us" className="nav--item" data-aos="fade-up" onClick={handleMenuClose}>About Us</Link>
-            </li>
-            <li className="nav--item--container list">
-              <Link to="/products" className="nav--item" data-aos="fade-down" onClick={handleMenuClose}>Products</Link>
-            </li>
-            <li className="nav--item--container list">
-              <Link to="/services" className="nav--item" data-aos="fade-up" onClick={handleMenuClose}>Services</Link>
-            </li>
-            <li className="nav--item--container list">
-              <Link to="/projects" className="nav--item" data-aos="fade-up" onClick={handleMenuClose}>Projects</Link>
-            </li>
-            <li className="nav--item--container list">
-              <Link to="/blogs" className="nav--item" data-aos="fade-down" onClick={handleMenuClose}>Blog</Link>
-            </li>
-            <li className="nav--item--container list">
-              <Link
-                to="/"
-                state={{ scrollToSection: 'name' }}
-                className="nav--item"
-                onClick={handleMenuClose}
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        {/* </CSSTransition> */}
+        >
+          <nav className={`nav--container ${toggle ? '' : 'hide--mob'}`}>
+            <ul className="nav--list un-list flex" data-aos="fade-left">
+              <li className="nav--item--container list">
+                <Link to="/" className="nav--item" data-aos="fade-up" onClick={handleMenuClose}>Home</Link>
+              </li>
+              <li className="nav--item--container list">
+                <Link to="/about-us" className="nav--item" data-aos="fade-up" onClick={handleMenuClose}>About Us</Link>
+              </li>
+              <li className="nav--item--container list">
+                <Link to="/products" className="nav--item" data-aos="fade-down" onClick={handleMenuClose}>Products</Link>
+              </li>
+              <li className="nav--item--container list">
+                <Link to="/services" className="nav--item" data-aos="fade-up" onClick={handleMenuClose}>Services</Link>
+              </li>
+              <li className="nav--item--container list">
+                <Link to="/projects" className="nav--item" data-aos="fade-up" onClick={handleMenuClose}>Projects</Link>
+              </li>
+              <li className="nav--item--container list">
+                <Link to="/blogs" className="nav--item" data-aos="fade-down" onClick={handleMenuClose}>Blog</Link>
+              </li>
+              <li className="nav--item--container list">
+                <Link
+                  to="/"
+                  state={{ scrollToSection: 'name' }}
+                  className="nav--item"
+                  onClick={handleMenuClose}
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </CSSTransition>
       </header>
 
-      {isMobile && <SideNav />}
       <Outlet />
     </>
   );
